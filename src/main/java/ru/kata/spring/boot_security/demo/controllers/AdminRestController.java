59885f385d776e7d8e08d62f878286424015ffd7
Ildar Gimadutdinov <tinkiwinki1443@gmail.com>
2025-04-28 18:46:06 +0400

    edit rest controller and logic service

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
@@ -1,7 +1,5 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,9 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.entity.User;
-import ru.kata.spring.boot_security.demo.exception_handlers.NotSuchFoundUserException;
 import ru.kata.spring.boot_security.demo.service.UserService;
-import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
 
 import java.security.Principal;
 import java.util.List;
@@ -24,7 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class AdminRestController {
-    UserService userService;
+    private final UserService userService;
 
     public AdminRestController(UserService userService) {
         this.userService = userService;
