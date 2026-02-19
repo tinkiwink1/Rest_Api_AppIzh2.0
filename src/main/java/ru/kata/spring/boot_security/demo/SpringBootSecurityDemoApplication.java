9f674924e327ef2dde0c59bc62efa066873224ac
ildar <tinkiwinki1443@gmail.com>
2026-02-20 00:21:57 +0400

    add module application springboot

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,10 +2,10 @@ package ru.kata.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
 public class SpringBootSecurityDemoApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
